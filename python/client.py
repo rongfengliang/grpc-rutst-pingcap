6ce334fa55cb1bd700e2c2afdd3cd841287170cd
dalongrong <1141591465@qq.com>
2018-03-09 14:20:02 +0800

    remove unused code

--- python/client.py
+++ python/client.py
@@ -5,7 +5,6 @@ import user_pb2_grpc
 def run():
     channel=grpc.insecure_channel("127.0.0.1:50051")
     stub=user_pb2_grpc.UserLoginStub(channel)
-    stub.Login()
     response =stub.Login(user_pb2.UserRequest(name="dalong",age="3333"))
     print(response.message)
 if __name__=='__main__':
