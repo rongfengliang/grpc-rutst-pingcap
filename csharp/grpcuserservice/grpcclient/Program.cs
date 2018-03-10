7e3073c12b0b8bd5d2d8f6f3fdc2d8fe483bd3d8
dalongrong <1141591465@qq.com>
2018-03-10 20:41:21 +0800

    add csharp code

---
+++ csharp/grpcuserservice/grpcclient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Grpc.Core;
+using userservice;
+namespace grpcclient
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Channel channel = new Channel("127.0.0.1:50052", ChannelCredentials.Insecure);
+
+            var client = new userservice.UserLogin.UserLoginClient(channel);
+            String user = "dalong";
+            var reply = client.Login(new UserRequest { Name = user, Age = "3eee" });
+            Console.WriteLine("hello from : " + reply.Message);
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
